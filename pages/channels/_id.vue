c0cc73b2ea54ad58649ec188ce73a7dff86a38ab
yuyaamano <nba.chako.nba@icloud.com>
2020-05-03 16:28:37 +0900

    messageIDをfirebaseから取得」

--- pages/channels/_id.vue
+++ pages/channels/_id.vue
@@ -12,11 +12,27 @@
 <script>
 import Messages from '~/components/Messages.vue'
 import ChatForm from '~/components/ChatForm.vue'
+import { db } from '~/plugins/firebase'
 
 export default {
   components: {
     Messages,
     ChatForm
+  },
+  data () {
+      return {
+          messages: []
+      }
+  },
+  mounted () {
+      const channelID = this.$route.params.id
+      db.collection('channels').doc(channelID).collection('messages').get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+            this.messages.push({id: doc.id, ...doc.data()})
+            })
+            console.log(this.messages) // 開発用に
+        })
   }
 }
 </script>
