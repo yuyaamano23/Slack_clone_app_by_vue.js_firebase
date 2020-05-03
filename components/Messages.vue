18de3c326045713b66819498ae68efbdfed8d2ff
yuyaamano <nba.chako.nba@icloud.com>
2020-05-03 12:58:28 +0900

    Message.vueの作成

--- components/Messages.vue
+++ components/Messages.vue
@@ -1,48 +1,23 @@
 <template>
  <div class="chats-container">
-   <div class="chat-container">
-     <div class="thumbnail-container">
-       <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-     </div>
-     <div class="message-container">
-       <div class="user-name">cohki0305</div>
-       <div class="message">今日も良い天気ですね</div>
-     </div>
-   </div>
-
-   <div class="chat-container">
-     <div class="thumbnail-container">
-       <img src="https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg" />
-     </div>
-     <div class="message-container">
-       <div class="user-name">cohki0305</div>
-       <div class="message">そうですねー</div>
-     </div>
-   </div>
+   <Message />
+   <Message />
  </div>
 </template>
 
+<script>
+import Message from '~/components/Message'
+
+export default {
+    components: {
+        Message
+    }
+    
+}
+</script>
+
 <style scoped>
 .chats-container {
- padding: 16px;
-}
-
-.chat-container {
- display: flex;
- padding: 8px;
-}
-
-.thumbnail-container {
- margin-right: 16px;
-}
-
-.thumbnail-container img {
- width: 50px;
- height: 50px;
- border-radius: 10px;
-}
-
-.user-name {
- font-weight: bold;
+    padding: 16px;
 }
 </style>
