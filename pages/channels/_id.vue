e889f79fca5bef8eadca711c214afa2849f91960
yuyaamano <nba.chako.nba@icloud.com>
2020-05-03 16:11:33 +0900

    チャンネルのIDにリンクを作成

---
+++ pages/channels/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="chats-layout">
+      <messages />
+    </div>
+    <div class="input-layout">
+      <ChatForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import Messages from '~/components/Messages.vue'
+import ChatForm from '~/components/ChatForm.vue'
+
+export default {
+  components: {
+    Messages,
+    ChatForm
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+}
+.chats-layout {
+  overflow: scroll;
+  height: 85%;
+}
+.input-layout {
+  height: 15%;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,12 @@
 <template>
   <div class="container">
-    <div class="chats-layout">
-      <messages />
-    </div>
-    <div class="input-layout">
-      <ChatForm />
-    </div>
   </div>
 </template>
 
 <script>
-import Messages from '~/components/Messages.vue'
-import ChatForm from '~/components/ChatForm.vue'
-// import { db } from '~/plugins/firebase'
 
-export default {
-  components: {
-    Messages,
-    ChatForm
-  }
-  // ,mounted () {
-  //   db.collection('channels').get()
-  //   .then((querySnapshot) => {
-  //     querySnapshot.forEach(function(doc) {
-  //       console.log(doc.id, " => ", doc.data())
-  //     })
-  //   })
-  // }
-}
 </script>
 
 <style scoped>
-.container {
-  height: 100%;
-}
-.chats-layout {
-  overflow: scroll;
-  height: 85%;
-}
-.input-layout {
-  height: 15%;
-}
 
 </style>
