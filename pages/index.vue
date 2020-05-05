b0368febbf6f2d67968a38d9cac64e081606c29b
yuyaamano <nba.chako.nba@icloud.com>
2020-05-05 22:28:18 +0900

    googleでのログイン機能を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="container">
+    <button v-on:click="login">ログイン</button>
   </div>
 </template>
 
 <script>
+import { firebase } from '~/plugins/firebase'
 
+export default {
+  methods: {
+    login () {
+      window.alert('ログインするよ')
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user
+          console.log(user)
+        }).catch((error) => {
+          window.alert(error)
+        })
+    }
+  }
+}
 </script>
 
 <style scoped>
