13303226bb54a79b8cb6d61bede02a4974f409cd
yuyaamano <nba.chako.nba@icloud.com>
2020-05-05 22:35:22 +0900

    element-uiの導入

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -7,6 +7,10 @@
 
 <script>
 import { db } from '~/plugins/firebase'
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 export default {
     data () {
